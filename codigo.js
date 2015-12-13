94ffc4385aa6b37da6b771e7b5a9621598f6c0e2
unknown <c3lg0n@gmail.com>
2015-12-13 07:55:05 -0430

    Primera versión del curso

---
+++ codigo.js
@@ -0,0 +1,7 @@
+var yo = {
+  nombre: 'celis',
+  edad: 26
+}
+if (true) {
+  alert(yo.nombre);
+}
---
+++ pokemon.js
@@ -0,0 +1,35 @@
+var turno = 1;
+
+var pikachu = {
+  vida: 100,
+  ataque: 55,
+  win: function () {
+    console.log('Ha ganado Pikachu');
+  }
+}
+
+var jigly = {
+  vida: 100,
+  ataque: 45,
+  win: function() {
+    console.log('Ha ganado Jigly');
+  }
+}
+
+while (pikachu.vida > 0 && jigly.vida > 0) {
+  if (turno == 1) {
+    jigly.vida = jigly.vida - pikachu.ataque;
+    turno = 2;
+    console.log('Ahora Jigly tiene ' + jigly.vida + ' puntos de vida');
+  } else {
+    pikachu.vida = pikachu.vida - jigly.ataque;
+    turno = 1;
+    console.log('Ahora Pikachu tiene '+ pikachu.vida + ' puntos de vida');
+  }
+}
+
+if (pikachu.vida <= 0) {
+  alert(jigly.win());
+} else {
+  alert(pikachu.win());
+}
